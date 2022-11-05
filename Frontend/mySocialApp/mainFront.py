f037806a1825a280a8ba8bc5f521d168ac2ad8e5
rdehor <106652324+rdehor@users.noreply.github.com>
2022-11-05 01:25:31 +0100

    Update mainFront.py

--- Frontend/mySocialApp/mainFront.py
+++ Frontend/mySocialApp/mainFront.py
@@ -1,5 +1,5 @@
 import flet
-from flet import Page, Text, TextField, Checkbox, Column, Row, FloatingActionButton, icons, Icon, Image, Container
+from flet import Page, Text, TextField, Checkbox, Column, Row, FloatingActionButton, icons, Icon, Image, Container, ElevatedButton
 from dotenv import dotenv_values
 
 environments = dotenv_values("../.env")
@@ -8,7 +8,7 @@ environments = dotenv_values("../.env")
 def main(page: Page):
     new_task = TextField(hint_text="Whats needs to be done?", expand=True)
     logo = Image(src=f"/logo.png", width=60, height=60)
-
+    temp = Image(src=f"/logo.png", width=10, height=10)
     navBar = Container(
         Column(
             controls=[
@@ -16,7 +16,12 @@ def main(page: Page):
                     controls=[
                         logo,
                         Text(environments.get("APPNAME"), size=20),
+                        ElevatedButton(text="Szukaj", color = "white", bgcolor = "TRANSPARENT", icon=temp),
+                        ElevatedButton(text="Organizacje", color = "white", bgcolor = "TRANSPARENT"),
+                        ElevatedButton(text="Zbiórki", color = "white", bgcolor = "TRANSPARENT"),
+                        ElevatedButton(text="Zaloguj się", bgcolor = "green", color = "white"),
                     ],
+                    alignment="center",
                 ),
             ],
         ),
@@ -28,6 +33,7 @@ def main(page: Page):
     page.padding=0
     page.horizontal_alignment = "center"
     page.add(navBar)
+
 
 
 flet.app(port=8000,
